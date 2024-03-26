776a56be680751b2a9666d55f87d98c0e2a9886b
unknown <429281-7>
2024-03-26 09:58:32 +0300

    init

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassLibrary1;
+
+public class MyClass
+{
+    public static void Check()
+    {
+        Console.WriteLine("Hello from published library");
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassLibrary1;
+
+static class Program
+{
+    public static void Main()
+    {
+        MyClass.Check();
+    }
+}
